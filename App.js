99050d7c3e85da6cc245903d2ebfe584d66f6a9c
Lau38 <54420468+Lau38@users.noreply.github.com>
2023-11-09 15:58:41 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { useColorScheme } from 'nativewind';
+import { StyleSheet, Switch, Text, View } from 'react-native';
+
+export default function App() {
+  const {colorScheme, toggleColorScheme} = useColorScheme();
+  return (
+    <View className="flex-1 justify-center items-center dark:bg-neutral-900 space-y-6">
+      <StatusBar styles={colorScheme=='dark'? 'light': 'dark'} />
+      <View className="flex-row justify-center items-center space-x-2">
+        <Text className="text-xl dark:text-white">Modo Oscuro</Text>
+        <Switch value={colorScheme=='dark'} onChange={toggleColorScheme} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
